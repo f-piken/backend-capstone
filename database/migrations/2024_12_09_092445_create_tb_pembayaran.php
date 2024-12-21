b3cd2d12304d2a53d997901dc82200ed108c02f0
f-piken <totpiken@outlook.com>
2024-12-21 17:53:33 +0700

    up to date

--- database/migrations/2024_12_09_092445_create_tb_pembayaran.php
+++ database/migrations/2024_12_09_092445_create_tb_pembayaran.php
@@ -15,15 +15,12 @@ return new class extends Migration
     {
         Schema::create('tb_pembayaran', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('nim'); // Nim seharusnya mengacu ke tb_mahasiswa
-            $table->string('nama'); // Nama sebaiknya tipe string
-            $table->integer('nominal'); // Nominal sebagai jumlah pembayaran
-            $table->string('metode_pembayaran'); // Metode sebagai string
+            $table->foreignId('nim')->constrained('tb_mahasiswa');
+            $table->string('nama');
+            $table->integer('nominal');
+            $table->string('metode_pembayaran');
             $table->enum('status_pembayaran', ['LUNAS', 'BELUM_LUNAS']);
             $table->timestamps();
-        
-            // Foreign key constraint
-            $table->foreign('nim')->references('nim')->on('tb_mahasiswa');
         });
         
     }
