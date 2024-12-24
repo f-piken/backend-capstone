4abf3607b8ee98f79e70a3cebe8fc58cd7ab3ec9
f-piken <totpiken@outlook.com>
2024-12-25 02:16:25 +0700

    email-pembayaran

--- routes/api.php
+++ routes/api.php
@@ -28,18 +28,21 @@ Route::middleware(['auth:api', 'role:admin'])->group(function () {
     Route::post('/send-admin', [ChatController::class, 'messageAdmin']);
     Route::put('/chats/{id}/approve', [ChatController::class, 'approveChat']);
     Route::put('/chats/{id}/end', [ChatController::class, 'endChat']);
-    
+
     Route::get('/pendaftaran', [MahasiswaController::class, 'index']);
     Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
 });
+
 Route::middleware(['auth:api', 'role:mahasiswa'])->group(function () {
 });
 
 Route::post('/daftar', [MahasiswaController::class, 'store']);
 
+Route::middleware(['auth','role:admin'])->group(function () {
+});
+Route::get('/get-messages/{pengirim}', [ChatController::class, 'getMessages']);
+
+
 Route::post('/send-message', [ChatController::class, 'messageUser']);
 Route::get('/get-messages/{pengirim}', [ChatController::class, 'getMessages']);
 Route::post('/buat-chat', [ChatController::class, 'mulaiChat']);
-
-Route::post('/kirim-email-pembayaran', [PembayaranController::class,'kirimEmailPembayaran']);
-
