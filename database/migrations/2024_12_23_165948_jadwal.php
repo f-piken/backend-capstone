895772443248d485530c341e7ea9d339faab2047
f-piken <totpiken@outlook.com>
2024-12-31 12:07:35 +0700

    baru

--- database/migrations/2024_12_23_165948_jadwal.php
+++ database/migrations/2024_12_23_165948_jadwal.php
@@ -15,16 +15,13 @@ class jadwal extends Migration
     {
         Schema::create('jadwal', function (Blueprint $table) {
             $table->id(); // Primary key
-            $table->unsignedBigInteger('mahasiswa_id'); // Foreign key ke tabel tb_mahasiswa
+            $table->unsignedBigInteger('mahasiswa_id')->references('id')->on('mahasiswa')->onDelete('cascade');
             $table->string('hari', 50); // Hari
             $table->time('waktu_mulai'); // Waktu mulai
             $table->time('waktu_selesai'); // Waktu selesai
             $table->string('mata_kuliah', 100); // Mata kuliah
             $table->string('ruang', 50); // Ruang
-            $table->timestamps(); // Timestamps (created_at dan updated_at)
-
-            // Tambahkan foreign key constraint
-            $table->foreign('mahasiswa_id')->references('id')->on('tb_mahasiswa')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
